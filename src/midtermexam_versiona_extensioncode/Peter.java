73af925d206104ff72a313643c161877755bb183
peter mina <minap@sheridanc.on.ca>
2022-06-24 14:22:57 -0400

    added Peter_Mina branch, peter.java with some comments

---
+++ src/midtermexam_versiona_extensioncode/Peter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author peter
+ */
+public class Peter {
+        // I like basketball
+    // I like food and TV
+    // My favourite player is Michael Jordan
+}
