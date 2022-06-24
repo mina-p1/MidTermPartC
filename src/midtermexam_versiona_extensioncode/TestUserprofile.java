d3414f16e641d4eb2c4d2d18c44ef56c19918576
peter mina <minap@sheridanc.on.ca>
2022-06-24 14:03:32 -0400

    added TestUserprofile java class

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author peter
+ */
+public class TestUserprofile {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Please Enter your name:");
+        String userID = in.next();
+        
+        
+        System.out.println(Arrays.toString(genres));
+        
+        System.out.println("Please select a genre from above: ");
+        String genre = in.next();
+        
+        System.out.println("Your User Profile has been created");
+        
+        
+    }
+}
